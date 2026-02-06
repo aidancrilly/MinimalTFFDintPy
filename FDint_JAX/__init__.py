d82cfd5a7bf4f8cad063a383c221a9df898f6de2
aidancrilly <a.crilly16@imperial.ac.uk>
2026-02-06 10:00:24 +0000

    Optional switch between vjp and jvp

--- FDint_JAX/__init__.py
+++ FDint_JAX/__init__.py
@@ -35,55 +35,112 @@ _fermi_dirac_integral_half_impl = build_fermi_dirac_integral_half(_JAX_BACKEND)
 _fermi_dirac_integral_three_half_impl = build_fermi_dirac_integral_three_half(_JAX_BACKEND)
 _inverse_fdi_half_impl = build_inverse_fermi_dirac_integral_half(_JAX_BACKEND)
 
+_VJP_VERSION = True
 
-@jax.custom_vjp
-def inverse_fermi_dirac_integral_half(x):
-    return _inverse_fdi_half_impl(x)
+# VJP version
+if _VJP_VERSION:
 
+    @jax.custom_vjp
+    def inverse_fermi_dirac_integral_half(x):
+        return _inverse_fdi_half_impl(x)
+    
+    @jax.custom_vjp
+    def fermi_dirac_integral_half(x):
+        return _fermi_dirac_integral_half_impl(x)
 
-def inverse_fd_half_fwd(x):
-    y = _inverse_fdi_half_impl(x)
-    return y, y
-
-
-def inverse_fd_half_bwd(res, g):
-    return (g / fermi_dirac_integral_minus_half(res),)
-
-
-inverse_fermi_dirac_integral_half.defvjp(inverse_fd_half_fwd, inverse_fd_half_bwd)
-
-
-@jax.custom_vjp
-def fermi_dirac_integral_half(x):
-    return _fermi_dirac_integral_half_impl(x)
+    @jax.custom_vjp
+    def fermi_dirac_integral_three_half(x):
+        return _fermi_dirac_integral_three_half_impl(x)
 
 
-def fermi_dirac_integral_half_fwd(x):
-    y = _fermi_dirac_integral_half_impl(x)
-    return y, fermi_dirac_integral_minus_half(x)
+    def inverse_fd_half_fwd(x):
+        y = _inverse_fdi_half_impl(x)
+        return y, y
 
 
-def FD_half_bwd(res, g):
-    return (res * g,)
+    def inverse_fd_half_bwd(res, g):
+        return (g / fermi_dirac_integral_minus_half(res),)
 
 
-fermi_dirac_integral_half.defvjp(fermi_dirac_integral_half_fwd, FD_half_bwd)
+    inverse_fermi_dirac_integral_half.defvjp(inverse_fd_half_fwd, inverse_fd_half_bwd)
+
+    def fermi_dirac_integral_half_fwd(x):
+        y = _fermi_dirac_integral_half_impl(x)
+        return y, fermi_dirac_integral_minus_half(x)
 
 
-@jax.custom_vjp
-def fermi_dirac_integral_three_half(x):
-    return _fermi_dirac_integral_three_half_impl(x)
+    def FD_half_bwd(res, g):
+        return (res * g,)
 
 
-def fermi_dirac_integral_three_half_fwd(x):
-    y = _fermi_dirac_integral_three_half_impl(x)
-    return y, fermi_dirac_integral_half(x)
+    fermi_dirac_integral_half.defvjp(fermi_dirac_integral_half_fwd, FD_half_bwd)
+
+    def fermi_dirac_integral_three_half_fwd(x):
+        y = _fermi_dirac_integral_three_half_impl(x)
+        return y, fermi_dirac_integral_half(x)
 
 
-def FD_three_halfs_bwd(res, g):
-    return (res * g,)
+    def FD_three_halfs_bwd(res, g):
+        return (res * g,)
 
 
-fermi_dirac_integral_three_half.defvjp(
-    fermi_dirac_integral_three_half_fwd, FD_three_halfs_bwd
-)
+    fermi_dirac_integral_three_half.defvjp(
+        fermi_dirac_integral_three_half_fwd, FD_three_halfs_bwd
+    )
+
+# JVPs
+else:
+
+    @jax.custom_jvp
+    def inverse_fermi_dirac_integral_half(x):
+        return _inverse_fdi_half_impl(x)
+    
+    @jax.custom_jvp
+    def fermi_dirac_integral_half(x):
+        return _fermi_dirac_integral_half_impl(x)
+
+    @jax.custom_jvp
+    def fermi_dirac_integral_three_half(x):
+        return _fermi_dirac_integral_three_half_impl(x)
+    
+    def inverse_fd_half_jvp(primals, tangents):
+        x, = primals
+        x_dot, = tangents
+        
+        y = _inverse_fdi_half_impl(x)
+        
+        gradient_at_y = fermi_dirac_integral_minus_half(y)
+        
+        y_dot = x_dot / gradient_at_y
+        
+        return y, y_dot
+
+    inverse_fermi_dirac_integral_half.defjvp(inverse_fd_half_jvp)
+
+    def fermi_dirac_integral_half_jvp(primals, tangents):
+        x, = primals
+        x_dot, = tangents
+        
+        y = _fermi_dirac_integral_half_impl(x)
+        
+        gradient = fermi_dirac_integral_minus_half(x)
+
+        y_dot = gradient * x_dot
+        
+        return y, y_dot
+
+    fermi_dirac_integral_half.defjvp(fermi_dirac_integral_half_jvp)
+
+    def fermi_dirac_integral_three_half_jvp(primals, tangents):
+        x, = primals
+        x_dot, = tangents
+
+        y = _fermi_dirac_integral_three_half_impl(x)
+
+        gradient = fermi_dirac_integral_half(x)
+
+        y_dot = gradient * x_dot
+        
+        return y, y_dot
+
+    fermi_dirac_integral_three_half.defjvp(fermi_dirac_integral_three_half_jvp)
